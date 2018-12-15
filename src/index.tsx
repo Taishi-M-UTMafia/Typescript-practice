2b555cb4174e037fa6393505b8c7c876bb1fc0d4
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-12-15 12:39:54 +0900

    Create test form

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // import { createStore, applyMiddleware } from 'redux';
+import TestForm from './components/test_form'
 
-import { App } from './components/app';
+// import { App } from './components/app';
 import reducers from './reducers';
 
 // const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
-    <App />
+    <TestForm />
   </Provider>,
   document.querySelector('.container')
 );
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 import ActiveBook from './reducer_active_book';
+import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
+  form: formReducer,
   books: BooksReducer,
   activeBook: ActiveBook
 });
